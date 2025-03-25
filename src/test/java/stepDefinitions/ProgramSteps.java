74918cfeda2e2fd9d87934483b50f227d75eb8f0
Madhuchhanda Sahu <madhuchhanda.sahu03@gmail.com>
2025-03-24 21:14:50 -0400

    step

--- src/test/java/stepDefinitions/ProgramSteps.java
+++ src/test/java/stepDefinitions/ProgramSteps.java
@@ -31,34 +31,11 @@ public class ProgramSteps {
 		programPlayload.ProgramResponse(method,endPoint);
 	}
 
-	@Then("Admin receives {int} Created Status with response body.")
-	public void admin_receives_created_status_with_response_body(int statuscode) {
-		Assert.assertEquals(programPlayload.response.getStatusCode(), statuscode);
-	}
+	
 
 	@When("Admin sends {string} HTTPS Request and  request Body with invalid endpoint")
 	public void admin_sends_https_request_and_request_body_with_invalid_endpoint(String string) {
 		programPlayload.requestWithInvalidEndpoint();
-	}
-
-	@Then("Admin receives {int} not found  Status with message and boolean success details")
-	public void admin_receives_not_found_status_with_message_and_boolean_success_details(int statuscode) {
-		Assert.assertEquals(programPlayload.response.getStatusCode(), statuscode);
-	}
-
-	@Then("Admin receives {int} Bad Request Status with message and boolean success details")
-	public void admin_receives_bad_request_status_with_message_and_boolean_success_details(int statuscode) {
-		 Assert.assertEquals(programPlayload.response.getStatusCode(), statuscode);
-	}
-
-	@Then("Admin receives {int} Method Not Allowed")
-	public void admin_receives_method_not_allowed(int statuscode) {
-		 Assert.assertEquals(programPlayload.response.getStatusCode(), statuscode);
-	}
-
-	@Then("Admin receives {int} Bad Request Status")
-	public void admin_receives_bad_request_status(int statuscode) {
-		 Assert.assertEquals(programPlayload.response.getStatusCode(), statuscode);
 	}
 
 	@Given("Admin creates POST Request for the LMS with invalid request body")
@@ -79,18 +56,29 @@ public class ProgramSteps {
 	
 	//*********************GET REQUEST********************************************
 	@Given("Admin creates GET Request for the LMS API")
-	public void admin_creates_get_request_for_the_lms_api() {
+	public void admin_creates_get_request_for_the_lms_api() throws IOException {
+		programPlayload.programGetRequest();
 	    
 	}
 
 	@When("Admin sends {string} HTTPS Request with {string} endpoint")
-	public void admin_sends_https_request_with_endpoint(String string, String string2) {
+	public void admin_sends_https_request_with_endpoint(String method, String endPoint) {
+		programPlayload.ProgramResponse(method,endPoint);
 	    
 	}
 
 	@Then("Admin receives {int} Status code")
-	public void admin_receives_status_code(int int1) {
-	    
+	public void admin_receives_status_code(int statuscode) {
+		Assert.assertEquals(programPlayload.response.getStatusCode(), statuscode);   
+	}
+	@When("Admin sends {string} HTTPS Request with invalid endpoint")
+	public void admin_sends_https_request_with_invalid_endpoint(String string) {
+		programPlayload.getRequestWithInvalidEndpoint();
+	}
+
+	@Given("Admin creates GET Request for the LMS API without Authorization")
+	public void admin_creates_get_request_for_the_lms_api_without_authorization() throws IOException {
+		programPlayload.getAllProgramWithoutAuthorization();
 	}
 
 
